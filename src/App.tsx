213573afad20624ae73df338a73bcfa9cb365973
Ali Enrique Briceño <alienriquebm@Mac-mini-de-Ali.local>
2025-05-19 12:01:26 -0400

    feat(ToDoApp): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4 bg-gray-100 w-full min-h-screen justify-center items-center flex flex-col">
+      <p>This is a simple React app with TypeScript.</p>
+      <p>Use this template to start the Digheontech's Todo app.</p>
+    </div>
+  );
+}
+
+export default App;
