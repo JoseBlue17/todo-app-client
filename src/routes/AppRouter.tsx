ded90b121909e7794d04c74b1859ff85fda15f6c
Oliver Velasquez <Oliver.vlz01@gmail.com>
2025-05-28 10:08:27 -0500

    feat(routing): Add protected route for home

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
+import ProtectedRoute from './ProtectedRoute';
 
 export default function AppRouter() {
   return (
@@ -8,7 +9,14 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
