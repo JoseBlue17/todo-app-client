5cad2d389eb6dca42fdc06b3b661edf5e9f217a3
JoseBlue17 <josemiguelramirezc0@gmail.com>
2025-06-13 16:09:59 -0500

    refactor(ListTodo): separation of logic and design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,11 @@
-import { useState } from 'react';
-import ListTodoHome from '../../components/ListTodoHome.tsx';
-import SearchBar from '../../components/SearchBar.tsx';
-import LogoAdd from '../../components/LogoAdd.tsx';
 import { useHome } from './useHome';
-import dayjs from 'dayjs';
-
-function getDueDateLabel(dueDate: string | Date) {
-  const today = dayjs().startOf('day');
-  const due = dayjs(dueDate).startOf('day');
-  const diff = due.diff(today, 'day');
-  if (diff === 0) return 'Due Today';
-  if (diff === 1) return 'Due Tomorrow';
-  if (diff === 2) return 'Due In 2 Days';
-  return `Due On ${dayjs(dueDate).format('MMM DD, YYYY')}`;
-}
+import MenuIcon from '../../components/MenuIcon.tsx';
+import SearchBar from '../../components/SearchBar.tsx';
+import AddIcon from '../../components/AddIcon.tsx';
+import TaskList from './TaskList';
 
 export default function Home() {
-  const { tasks, loading, error } = useHome();
-  const [checkedTasks, setCheckedTasks] = useState<Record<string, boolean>>({});
-
-  const handleCheck = (taskId: string) => {
-    setCheckedTasks(prev => ({
-      ...prev,
-      [taskId]: !prev[taskId],
-    }));
-  };
+  const { tasks, loading, error, handleCheck } = useHome();
 
   return (
     <div className="relative w-full min-h-screen">
@@ -34,7 +15,7 @@ export default function Home() {
         <div className="w-full max-w-[396px] lg:max-w-[938px] min-h-screen lg:min-h-[697px] bg-white lg:rounded-[20px] lg:shadow-[0px_2px_30px_rgba(0,0,0,0.1)] lg:pt-6 lg:pb-6 lg:px-6 py-5 relative">
           <div className="w-full flex flex-col lg:flex-row gap-[18px] items-center">
             <div className="hidden lg:flex lg:flex-1 items-center gap-4">
-              <ListTodoHome className="w-[21px] h-[20px] text-black" />
+              <MenuIcon className="w-[21px] h-[20px] text-black" />
             </div>
 
             <div className="hidden lg:flex flex-1 justify-center items-center text-center">
@@ -45,7 +26,7 @@ export default function Home() {
 
             <div className="relative flex lg:hidden w-full items-center  h-[24px]">
               <div className="absolute left-0">
-                <ListTodoHome className="w-[21px] h-[20px] text-[#C18EC7]" />
+                <MenuIcon className="w-[21px] h-[20px] text-[#C18EC7]" />
               </div>
 
               <div className="absolute inset-x-0 text-center">
@@ -55,24 +36,18 @@ export default function Home() {
               </div>
             </div>
             <div className="w-full lg:hidden">
-              <div
-                className="border-b border-[#EFEFEF] -mx-4 px-0"
-                style={{ pointerEvents: 'none' }}
-              />
+              <div className="border-b border-[#EFEFEF] -mx-4 px-0 pointer-events-none" />
             </div>
 
             <div className="flex flex-row lg:flex-1 w-full lg:w-auto gap-3 lg:mt-0 justify-end items-center">
               <div className="w-full lg:w-[122px]">
-                <SearchBar className="w-full h-[37px] rounded-[8px] border border-gray-300" />
+                <SearchBar />
               </div>
-              <LogoAdd className="w-[20px] h-[20px]" />
+              <AddIcon className="w-[20px] h-[20px]" />
             </div>
           </div>
 
-          <div
-            className="border-b border-[#EFEFEF] pb-4 -mx-4 lg:-mx-6 px-0 lg:px-6 hidden lg:block"
-            style={{ pointerEvents: 'none' }}
-          />
+          <div className="border-b border-[#EFEFEF] pb-4 -mx-4 lg:-mx-6 px-0 lg:px-6 hidden lg:block pointer-events-none" />
 
           <section className="lg:pt-4 px-0 flex flex-col gap-y-0">
             {loading && <p>Cargando tareas...</p>}
@@ -80,86 +55,7 @@ export default function Home() {
             {!loading && !error && tasks.length === 0 && (
               <p className="text-gray-500">No hay tareas aún.</p>
             )}
-
-            {!loading &&
-              !error &&
-              tasks.map((task, idx) => (
-                <div
-                  key={task._id}
-                  className={`mb-5 border-b border-[#EFEFEF] pb-4 -mx-4 px-4 lg:px-6 lg:-mx-6 ${
-                    idx === 0 ? ' pt-5 lg:pt-0' : ''
-                  }`}
-                >
-                  <div className="flex items-start gap-4 ">
-                    <input
-                      type="checkbox"
-                      checked={checkedTasks[task._id] || false}
-                      onChange={() => handleCheck(task._id)}
-                      className="w-[16.5px] h-[16.5px] cursor-pointer mt-[1.4px]"
-                    />
-                    <div
-                      className="w-[20px] h-[20px] rounded-[4px]"
-                      style={{
-                        backgroundColor: checkedTasks[task._id] ? '#A174CA' : task.category,
-                      }}
-                    />
-                    <div className="flex-1">
-                      <div className="hidden lg:grid lg:grid-cols-[2fr_1.5fr_3fr_1fr] lg:gap-4 items-center">
-                        <p
-                          className={`text-[14px] font-normal text-[#4A4A4A] ${
-                            checkedTasks[task._id] ? 'line-through' : ''
-                          }`}
-                        >
-                          {task.title}
-                        </p>
-                        <p
-                          className={`text-[14px] font-normal ${
-                            checkedTasks[task._id] ? 'text-[#BD8BC8]' : 'text-[#4A4A4A]'
-                          }`}
-                        >
-                          {getDueDateLabel(task.dueDate)}
-                        </p>
-                        <p className="text-[14px] font-normal leading-[120%] text-[#0620618A] truncate">
-                          {task.description}
-                        </p>
-                        <span className="text-[14px] font-medium leading-[120%] tracking-[-0.01em] text-[#0620618A] text-right">
-                          {dayjs(task.dueDate).format('hh:mm A')}
-                        </span>
-                      </div>
-                      <div className="lg:hidden flex flex-col gap-1">
-                        <div className="flex flex-row justify-between items-start">
-                          <p
-                            className={`text-[14px] font-normal text-[#4A4A4A] ${
-                              checkedTasks[task._id] ? 'line-through' : ''
-                            }`}
-                          >
-                            {task.title}
-                          </p>
-                          <p
-                            className={`text-[14px] font-normal text-right ${
-                              checkedTasks[task._id] ? 'text-[#BD8BC8]' : 'text-[#4A4A4A]'
-                            }`}
-                          >
-                            {getDueDateLabel(task.dueDate)}
-                          </p>
-                        </div>
-
-                        <div className="flex flex-row items-center gap-2">
-                          <p className="text-[14px] font-normal leading-[120%] text-[#0620618A] w-[70%]">
-                            {task.description}
-                          </p>
-                        </div>
-
-                        <div>
-                          <p className="text-[14px] font-medium leading-[120%] tracking-[-0.01em] text-[#0620618A]">
-                            {dayjs(task.dueDate).format('hh:mm A')}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
+            {!loading && !error && <TaskList tasks={tasks} handleCheck={handleCheck} />}
           </section>
         </div>
       </div>
--- src/pages/Home/useHome.tsx
+++ src/pages/Home/useHome.tsx
@@ -1,35 +1,46 @@
 import { useEffect, useState } from 'react';
-import authService from '../../services/authService';
+import { taskService } from '../../services/taskService';
+import { getDueDateLabel, getDueHourLabel } from '../../helpers/get-due-date-label';
 
-interface Task {
+export type TaskUI = {
   _id: string;
   title: string;
   description: string;
-  dueDate: string; 
-  category: string; 
-  completed: boolean; 
-  userId?: string;
-  createdAt?: string;
-  updatedAt?: string;
-}
+  dueDateLabel: string;
+  dueHourLabel: string;
+  checked: boolean;
+  category: string;
+};
 
 export function useHome() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<TaskUI[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const data = await authService.getTasks();
+        const data = await taskService.getTasks();
         console.log('Tareas obtenidas:', data);
 
-        const tasksWithDefaultColor = data.tasks.map((task: Task) => ({
+        type TaskFromApi = {
+          _id: string;
+          title: string;
+          description: string;
+          dueDate: string;
+          completed: boolean;
+          category?: string;
+        };
+
+        const tasksUI: TaskUI[] = data.tasks.map((task: TaskFromApi) => ({
           ...task,
-          category: task.category ? task.category : '#FF0202',
+          dueDateLabel: getDueDateLabel(task.dueDate),
+          dueHourLabel: getDueHourLabel(task.dueDate),
+          checked: !!task.completed,
+          category: task.category?.trim() ? task.category : '#FF0202',
         }));
 
-        setTasks(tasksWithDefaultColor);
+        setTasks(tasksUI);
       } catch (error) {
         console.error('Error al obtener tareas:', error);
         setError('No se pudieron cargar las tareas.');
@@ -41,9 +52,16 @@ export function useHome() {
     fetchTasks();
   }, []);
 
+  const handleCheck = (taskId: string) => {
+    setTasks(prev =>
+      prev.map(task => (task._id === taskId ? { ...task, checked: !task.checked } : task)),
+    );
+  };
+
   return {
     tasks,
     loading,
     error,
+    handleCheck,
   };
 }
